172bc59601aab5a8864f50148d2ec7a7ac3bb190
iamyutasugiyama <iamyutasugiyama@gmail.com>
2026-03-07 17:05:47 +0900

    new post

--- build.js
+++ build.js
@@ -1,30 +1,42 @@
 const fs = require("fs");
 const { marked } = require("marked");
 
+// markdownフォルダの中を読む
 const files = fs.readdirSync("markdown");
 
 let postList = "";
 
+// 1つずつ記事を処理
 files.forEach(file => {
 
   const markdown = fs.readFileSync(`markdown/${file}`, "utf8");
 
-  const html = marked(markdown);
+  const lines = markdown.split("\n");
 
-  const title = markdown.split("\n")[0].replace("# ", "");
+  const title = lines[0].replace("title: ", "");
+  const date = lines[1].replace("date: ", "");
+  const description = lines[2].replace("description: ", "");
+
+  const htmlContent = marked(markdown);
 
   const htmlName = file.replace(".md", ".html");
 
-  const template = `
+  // 記事ページ
+  const postTemplate = `
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <link rel="stylesheet" href="../css/style.css">
+<title>${title}</title>
 </head>
+
 <body>
 
-${html}
+<h1>${title}</h1>
+<p>${date}</p>
+
+${htmlContent}
 
 <a href="../index.html">トップへ戻る</a>
 
@@ -32,23 +44,29 @@ ${html}
 </html>
 `;
 
-  fs.writeFileSync(`posts/${htmlName}`, template);
+  fs.writeFileSync(`posts/${htmlName}`, postTemplate);
 
+  // 記事一覧
   postList += `
 <div class="post-card">
 <h3><a href="posts/${htmlName}">${title}</a></h3>
+<p>${date}</p>
+<p>${description}</p>
 </div>
 `;
 
 });
 
+// トップページ
 const indexTemplate = `
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <link rel="stylesheet" href="css/style.css">
+<title>My Blog</title>
 </head>
+
 <body>
 
 <header>
